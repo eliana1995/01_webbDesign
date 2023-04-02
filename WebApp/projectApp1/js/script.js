0f73f94bd0468adee14d33b562a6c403f9982143
eliana <113516213+eliana1995@users.noreply.github.com>
2023-04-02 20:22:26 +0200

    Add files via upload

---
+++ WebApp/projectApp1/js/script.js
@@ -0,0 +1,11 @@
+try {
+    const toggleBtn = document.querySelector('[data-option="toggle"]')
+    toggleBtn.addEventListener('click', function() {
+        const element = document.querySelector(toggleBtn.getAttribute('data-target'))
+        
+        if (!element.classList.contains('hide'))
+            element.classList.add('hide')
+        else 
+        element.classList.remove('hide')
+    })    
+} catch {}
